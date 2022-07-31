926941cfbe94395fe244ded18e44cd17bb12389f
Nox225 <104398198+Nox225@users.noreply.github.com>
2022-07-31 18:56:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import App from './App'
 import { AppProvider } from './context'
 import './Style/beer-style.css'
 
-//ReactDOM.render(<App />, document.getElementById('root'))
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
